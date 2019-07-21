100a6e1d757192aaaef7ac79205116e8c99dd9dd
Matt Coy <fluffytoycoy@gmail.com>
2019-07-20 23:28:50 -0500

    added imgs for service models and finished service grid section

--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -11,9 +11,43 @@ function Home(){
           <div className="bg"></div>
         </div>
         <div className="home-body">
-          <div className="container">
-            <div className="about-us">
-              <h2>About Us</h2>
+          <div className="">
+            <div className="container about-us">
+              <div>
+              <h2>Its All About the Details</h2>
+                </div>
+                <img src="/img/family.png"/>
+                <p>Since 2006, Chamberlin Heating & Air Conditioning
+                has been recognized as a top HVAC Contractor in Muscatine.
+                 We’ve provided clients with a wide range of contracting
+                 services for all their remodeling and repairing needs.
+                  Whether you’re looking to renovate your existing space or
+                  add a whole new extension, we’re happy to help you achieve
+                  the results you seek. Contact us today to receive a free
+                  estimate.</p>
+
+            </div>
+            <div className="space">
+            </div>
+            <div className="space">
+            </div>
+            <div className="service-section">
+              <div className="container">
+                <div>
+                <h2>We Service All Makes & Models</h2>
+                <div className="service-model-icons">
+                  <img src="/img/amana.png"/>
+                  <img src="/img/american-standard.png"/>
+                  <img src="/img/ao-smith.png"/>
+                  <img src="/img/carrier.png"/>
+                  <img src="/img/lennox.png"/>
+                  <img src="/img/rheem.png"/>
+                  <img src="/img/state-water.png"/>
+                  <img src="/img/york.png"/>
+                </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
