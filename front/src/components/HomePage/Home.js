673bca56584e6c6b76682ab3c920b3929c69d599
Matt Coy <fluffytoycoy@gmail.com>
2019-07-20 20:57:43 -0500

    hero finished moving to section 2

--- front/src/components/HomePage/Home.js
+++ front/src/components/HomePage/Home.js
@@ -2,18 +2,13 @@ import React from 'react';
 import './Home.scss';
 
 function Home(){
-
-
-
-
-
-
     return (
       <div id="home">
         <div className="hero">
-          <h1>Muscatines trusted <span>HVAC</span> experts</h1>
-          <div className="bg">
-          </div>
+            <h1>Muscatines trusted <span>HVAC</span> experts</h1>
+            <h3>Contact us for a free estimate today!</h3>
+            <div className="contact-btn">Contact Us</div>
+          <div className="bg"></div>
         </div>
         <div className="home-body">
           <div className="container">
@@ -24,7 +19,6 @@ function Home(){
         </div>
       </div>
     );
-
 }
 
 export default Home;
